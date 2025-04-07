156d5baab8c0db69aeee43e053d581448603bb7a
Szász István Zoltán <istvanszasz99@gmail.com>
2025-04-07 12:45:03 +0200

    Models and stored procedures

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(default=6)
+    booking_date = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f"{self.name}: {self.booking_date}"
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(default=5)
+
+    def __str__(self):
+        return f"{self.title}: ${self.price:.2f}"
